326bc01da645982492857220f7a6d74c78b957e2
mert.alptekin <mert.alptekin@neomina.com>
2022-11-03 14:25:33 +0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ReturnStatement } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TestApp';
+  title: string = 'TestApp'; // property
+  name: any; // any herhangi bir tip yan tarafındaki atamaya göre değişiklik gösterir.
+  show:boolean = false;
+
+  names: any[] = [
+    { name: 'ali', surname: 'tan' },
+    { name: 'ahmet', surname: 'tan2' }
+  ]
+
+  cities:any[] = ['izmir','kocaeli','istanbul']
+  selectedCity = 'izmir';
+  disabled:boolean = false;
+
+
+  showAlert(): void { // method
+    alert('asdsad')
+  }
+
+  getName(): string {
+    return "name";
+  }
 }
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  productEndpointUrl:'https:test-app/api/products'
+};
